cad77c25d9b75b25dc22d7f7df5f79c2c90cc186
thibaut <t.chamard@iwoca.co.uk>
2020-12-05 17:54:01 +0000

    Add day 5.

---
+++ 5/solutions.py
@@ -0,0 +1,29 @@
+import re
+from collections import Counter
+from utils.abstract import SolutionsAbstract
+
+
+class SolutionsDay5(SolutionsAbstract):
+    code_to_binary = {
+        'F': '0',
+        'B': '1',
+        'L': '0',
+        'R': '1',
+    }
+    
+    @staticmethod
+    def prepare_data(inputs):
+        return inputs.splitlines()
+    
+    @classmethod
+    def _code_to_int(cls, code):
+        return int(''.join([cls.code_to_binary[el] for el in code]), 2)
+
+    def part_1(self):
+        return max(map(self._code_to_int, self.data))
+
+    def part_2(self):
+        seats = set(map(self._code_to_int, self.data))
+        for seat in seats:
+            if all([seat + 1 not in seats, seat + 2 in seats]):
+                return seat + 1
